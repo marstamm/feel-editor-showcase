ff1d60773cc81cecfc761ce3628eab85d1d3c4aa
Martin Stamm <martin.stamm@camunda.com>
2022-06-03 11:34:55 +0200

    chore: update to latestes dev

--- webpack.config.js
+++ webpack.config.js
@@ -6,7 +6,7 @@ module.exports = {
   mode: 'development',
   entry: './src/app.js',
   output: {
-    path: path.resolve(__dirname, 'public'),
+    path: path.resolve(__dirname, 'docs'),
     filename: 'app.js'
   },
   module: {
