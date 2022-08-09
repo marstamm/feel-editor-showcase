e8ecb5672a10d5422c8f6dea94ac85c519fea9f9
Martin Stamm <martin.stamm@camunda.com>
2022-08-09 16:54:02 +0200

    add copy/paste

--- webpack.config.js
+++ webpack.config.js
@@ -6,7 +6,7 @@ module.exports = {
   mode: 'development',
   entry: './src/app.js',
   output: {
-    path: path.resolve(__dirname, 'docs/snapshots/2022-07-19'),
+    path: path.resolve(__dirname, 'docs/snapshots/2022-08-09'),
     filename: 'app.js'
   },
   module: {
